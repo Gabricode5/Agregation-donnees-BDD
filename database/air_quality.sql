7ee1d6bac443e8d593c5ed628f300a3ac1f8d4a9
marimoun <marimounmed@gmail.com>
2025-10-22 19:13:37 +0200

    database tabes & cities dataset

---
+++ database/air_quality.sql
@@ -0,0 +1,19 @@
+
+CREATE TABLE AirQuality (
+    id serial PRIMARY KEY,
+    id_city_fk INT not null,
+    date date NOT NULL,
+    pm25 DECIMAL(5,2),
+    pm10 DECIMAL(5,2),
+    o3 DECIMAL(5,2),
+    no2 DECIMAL(5,2),
+    so2 DECIMAL(5,2),
+    co DECIMAL(5,2),
+    aqi INT, 
+    AQI_Category VARCHAR(50),
+    constraint fk_city 
+    	foreign key(id_city_fk)
+    	references City(id)
+)
+
+
--- services/lile_handler.py
+++ services/lile_handler.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import pandas  as  pd 
 
 try:
@@ -21,4 +20,23 @@ print("============= afficher les city =================")
 for ville,latitude , longitude in zip(df["city"].unique(), df["latitude"].unique(), df["longitude"].unique()):
     print(ville, "=> { latitude: ", latitude, ", longitude:", longitude, "}")
 
+
+city_df = pd.DataFrame({"city":df["city"].unique(),
+                        "latitude":df["latitude"].unique(),
+                       "longitude":df["longitude"].unique(),
+                       "id":list(range(1,df["city"].nunique()+1))
+                       })
+cities_ids = []
+for idx,ligne in df.iterrows():
+    cities_ids.append(
+        list(city_df[city_df["city"] == df.iloc[idx]["city"]]["id"].unique())[0]
+        )
+
+df["id_city_fk"] = cities_ids
+
+df = df.drop(columns=['city' , 'latitude','longitude','temperature_c', 'humidity_percent',
+'wind_speed_mps', 'month', 'day', 'year', 'day_of_week', 'hour',
+'is_weekend', 'day_of_year', 'season'])
+
 df.to_csv("C:/Users/marim/Downloads/Air_Quality_out.csv", index=False)
+city_df.to_csv("C:/Users/marim/Downloads/cities.csv", index=False)
