0f0d695ff2acbbc194d366bbdd5872b94abae4f9
Hopeyaaa <nadya.ouhssain29@gmail.com>
2025-10-27 17:40:37 +0100

    Update us_population.sql

--- database/us_population.sql
+++ database/us_population.sql
@@ -1,6 +1,6 @@
 CREATE TABLE US_Population (
     id SERIAL PRIMARY KEY,
-    city_id VARCHAR(50),
+    city_id INT NOT NULL,
     population INT,
     CONSTRAINT fk_city_population
     FOREIGN KEY (city_id) 
