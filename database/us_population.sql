6b5a2cbdb1dcb6d8d68308832a3df393c95922dc
Hopeyaaa <nadya.ouhssain29@gmail.com>
2025-10-27 17:37:27 +0100

    fix(schema/cleanup): Finalisation de la normalisation SQL et suppression des anciens scripts d'extraction.

--- database/us_population.sql
+++ database/us_population.sql
@@ -2,7 +2,5 @@ CREATE TABLE US_Population (
     id SERIAL PRIMARY KEY,
     city_id INT NOT NULL,
     population INT,
-    CONSTRAINT fk_city_population
-    FOREIGN KEY (city_id) 
-    REFERENCES City (id)
+    CONSTRAINT fk_city_population FOREIGN KEY (city_id) REFERENCES City (id)
 );
